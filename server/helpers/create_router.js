980520aebfef76c4a9bbd42d42578bc25f2a5874
Graeme Henderson <graeme.189@gmail.com>
2019-08-13 15:04:47 +0100

    MVP complete

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,6 +5,7 @@ const createRouter = function (collection) {
 
   const router = express.Router();
 
+// INDEX
   router.get('/', (req, res) => {
     collection
       .find()
@@ -17,6 +18,7 @@ const createRouter = function (collection) {
       });
   });
 
+// SHOW ONE
   router.get('/:id', (req, res) => {
     const id = req.params.id;
     collection
@@ -28,6 +30,18 @@ const createRouter = function (collection) {
         res.json({ status: 500, error: err });
       });
   });
+
+// CREATE
+  router.post('/', (req, res) => {
+    collection
+    .insertOne(req.body)
+    .then((result) => res.json(result.ops[0]))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error:err});
+    })
+  })
 
   return router;
 };
